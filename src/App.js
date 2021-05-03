b348068b4a40186bd58766c5f25bbef583051b2a
smit raval <smith29raval@gmail.com>
2021-05-03 23:14:53 +0530

    Working with cryptokitties

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
-import abi from './ERC721.abi.json';
+//import abi from './ERC721.abi.json';
 import Web3 from 'web3';
+//const erc721 = require("@0xcert/ethereum-erc721/build/erc721.json").ERC721;
+import { OpenSeaPort, Network } from 'opensea-js';
 
 function App() {
   // const { status, connect, account } = useMetaMask();
@@ -30,7 +32,27 @@ function App() {
     const web3 = window.web3
     // Load account
     const accounts = await web3.eth.getAccounts()
-    setAccount(accounts[0]);
+    const userAccount = accounts[0];
+    setAccount(userAccount);
+    
+    const seaport = new OpenSeaPort(window.web3.currentProvider, {
+      networkName: Network.Main
+    });
+
+    const cryptoAsset = {
+      tokenAddress: "0x06012c8cf97bead5deae237070f9587f8e7a266d", // string
+      tokenId: "1" // string | number | null
+    };
+
+    const balance = await seaport.getAssetBalance({
+      accountAddress: userAccount, // string
+      asset: cryptoAsset // Asset
+    });
+    
+    const ownsKitty = balance.greaterThan(0);
+
+    console.log("check it kitties", ownsKitty);
+
 
     //const networkId = await web3.eth.net.getId();
     //console.log("check id", networkId);
@@ -38,11 +60,12 @@ function App() {
     // if(networkData) {
     //   const abi = Color.abi
     //   const address = networkData.address
-    const contract = new web3.eth.Contract(abi, "0x495f947276749Ce646f68AC8c248420045cb7b5e");
-    console.log("check contract", contract);
+    //const contract = new web3.eth.Contract(erc721.abi, "0x495f947276749Ce646f68AC8c248420045cb7b5e");
+    //const totalSupply = await contract.methods.totalSupply().call();
+    
     //   this.setState({ contract })
-    const totalSupply = await contract.methods.totalSupply().call();
-    console.log("total supply", totalSupply);
+    // const totalSupply = await contract.methods.totalSupply().call();
+    // console.log("total supply", totalSupply);
     //   this.setState({ totalSupply })
     //   // Load Colors
     //   for (var i = 1; i <= totalSupply; i++) {
