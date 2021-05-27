1b658141fb37283537e309d92d79ab7d9e1536ac
MonkeyTiger <monkeyflytiger@gamil.com>
2021-05-28 01:42:45 +1000

    Fix style of navbar

--- src/components/Common/Header/Navbar.js
+++ src/components/Common/Header/Navbar.js
@@ -1,14 +1,21 @@
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const links = [
+    {title: 'Your NFTs', to: '/gallery'},
+    {title: 'Wallet', to: '/'},
+    {title: 'Connet Metamask', to: '/'},
+    {title: 'Account Settings', to: '/'},
+    {title: 'Help', to: '/'},
+  ];
+
+  const linkList = links.map(link => {
+    return <li><Link to={link.to}>{link.title}</Link></li>
+  });
   return (
     <div className="navbar">
       <ul>
-        <li><Link to="/gallery">Your NFTs</Link></li>
-        <li><Link to="/">Wallet</Link></li>
-        <li><Link to="/">Connet Metamask</Link></li>
-        <li><Link to="/">Account Settings</Link></li>
-        <li><Link to="/">Help</Link></li>
+        {linkList}
       </ul>
     </div>
   );
