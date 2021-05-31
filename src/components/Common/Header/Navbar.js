caab19dbc6c9de2eea81ee11d98c42aa5f0f533d
selimerunkut <se@cypherx.tech>
2021-05-31 13:37:12 +0200

    added PrivacyPolicy and Help Page

--- src/components/Common/Header/Navbar.js
+++ src/components/Common/Header/Navbar.js
@@ -3,10 +3,9 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   const links = [
     {title: 'Your NFTs', to: '/gallery'},
-    {title: 'Wallet', to: '/'},
-    {title: 'Connet Metamask', to: '/'},
-    {title: 'Account Settings', to: '/'},
-    {title: 'Help', to: '/'},
+    {title: 'Homepage', to: '/'},
+    {title: 'Connect Metamask', to: '/'},
+    {title: 'Help', to: '/help'},
   ];
 
   const linkList = links.map((link, i) => {
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,8 @@ import Layout from './components/Layout';
 
 import Home from './components/Home';
 import Gallery from './components/Gallery';
+import PrivacyPolicy from './components/PrivacyPolicy';
+import Help from './components/Help';
 
 export default () => {
     return (
@@ -20,6 +22,16 @@ export default () => {
             <Route exact path="/gallery">
                 <Layout>
                     <Gallery />
+                </Layout>
+            </Route>
+            <Route exact path="/privacy-policy">
+                <Layout>
+                    <PrivacyPolicy />
+                </Layout>
+            </Route>
+            <Route exact path="/help">
+                <Layout>
+                    <Help />
                 </Layout>
             </Route>
         </Switch>
