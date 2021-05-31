ea333124353557a5e283227946fa77c948e6f170
MonkeyTiger <monkeyflytiger@gamil.com>
2021-05-31 18:00:41 +1000

    Fix autoselect gallery image

--- src/components/Gallery/Nfts.js
+++ src/components/Gallery/Nfts.js
@@ -1,19 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
-const Nfts = ({ type, images, selectedImage, saveImage }) => {
+const Nfts = ({ type, images, saveImage }) => {
   const [img, setImg] = useState(null);
 
   const selectImage = (el) => {
     setImg(el);
     saveImage(el);
   }
-
-  useEffect(() => {
-    if(!selectedImage && images.length > 0) {
-      setImg(images[0]);
-      saveImage(images[0]);
-    }
-  }, []);
 
   return (
     <div className={`nfts ${type}`}>
